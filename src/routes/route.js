746ba1c4d9d42b6084bcf88a7930a1a5bcbefdeb
Dolly <showdolly100@gmail.com>
2022-04-07 23:40:51 +0530

    yes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,33 +1,52 @@
 const express = require('express');
-const logger = require('./logger')
+// const loggermodule = require('./logger')
+
+// const helpermodule = require('../util/helper')
+// const formattermodule=require('../validator/formatter')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('I am inside the first route handler')
-    console.log('The endpoint value is', logger.endpoint)
-    console.log('Calling log function')
-    logger.logging()
-    res.send('My first ever api!')
+// router.get('/test-me', function (req, res) {
+    
+// loggermodule.welcomeMessage()
+//     res.send('My first ever api!')
+// });
+
+// router.get('/test-me2', function (req, res) {
+//     helpermodule.printtodaydate()
+//    // console.log('I am inside the second route handler')
+//     res.send('My second ever api!')
+// });
+
+
+// router.get('/test-me5', function (req, res) {
+//     formattermodule.trimCon()
+//     res.send('My final ever api!')
+// });
+
+// router.get('/test-me3', function (req, res) {
+//     res.send('My first ever api!')
+// });
+//problem1
+router.get('/movies', function (req, res) {
+    let arr=["rang de basanti","the shining","lord of the rings","bartman begins"]
+
+    res.send(arr)
 });
 
-router.get('/test-me2', function (req, res) {
-    console.log('I am inside the second route handler')
-    res.send('My second ever api!')
-});
 
 
-router.get('/test-me5', function (req, res) {
-    res.send('My final ever api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My first ever api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My first ever api!')
-});
-
-module.exports = router;
 // adding this comment for no reason
+//problem 2
+router.get('/i/:id', function (req, res) {
+    let a=req.params.id
+    let array=["rang de basanti","the shining","lord of the rings","bartman begins"]
+    if(a>=array.length){
+
+        res.send("invalid input")
+    }else{
+        res.send(array[a])
+    }   
+
+})
+module.exports = router;
